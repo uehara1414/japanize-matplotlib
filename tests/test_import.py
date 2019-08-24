44c9d1775f54ea7059fe8f79dc4c41a34adc62af
uehara <b1015062@fun.ac.jp>
2019-08-24 14:43:42 +0900

    Update test

--- tests/test_import.py
+++ tests/test_import.py
@@ -4,7 +4,7 @@ import unittest
 class ImportTest(unittest.TestCase):
 
     def test_import(self):
-        import japanize_matplotlib
+        from .. import japanize_matplotlib
 
 
 if __name__ == '__main__':

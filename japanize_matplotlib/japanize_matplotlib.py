945407ff158048744ba79ae959ad5702e2b99910
pesuchin <gp11a110@oecu.jp>
2020-05-04 12:20:35 +0900

    avoid deprecation warning in Matplotlib 3.2: font_manager.createFontList()

--- japanize_matplotlib/japanize_matplotlib.py
+++ japanize_matplotlib/japanize_matplotlib.py
@@ -2,6 +2,7 @@ import os
 
 import matplotlib
 from matplotlib import font_manager
+from distutils.version import LooseVersion
 
 FONTS_DIR = 'fonts'
 FONT_NAME = "IPAexGothic"
@@ -12,8 +13,13 @@ def japanize():
     font_dir_path = get_font_path()
     font_dirs = [font_dir_path]
     font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
-    font_list = font_manager.createFontList(font_files)
-    font_manager.fontManager.ttflist.extend(font_list)
+    is_support_createFontList = LooseVersion(matplotlib.__version__) < '3.2'
+    if is_support_createFontList:
+        font_list = font_manager.createFontList(font_files)
+        font_manager.fontManager.ttflist.extend(font_list)
+    else:
+        for fpath in font_files:
+            font_manager.fontManager.addfont(fpath)
     matplotlib.rc('font', family=FONT_NAME)
 
 

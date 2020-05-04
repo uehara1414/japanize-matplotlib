09f05a961784634cc77972ad9b3d3f8f191f8473
wakamezake <hotwater1367@gmail.com>
2020-05-04 16:11:41 +0900

    pytest

--- tests/test_japanize_matplotlib.py
+++ tests/test_japanize_matplotlib.py
@@ -1,23 +1,19 @@
 import os
-import unittest
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
 from japanize_matplotlib import get_font_path, get_font_ttf_path
 
 
-class TestJapanizeMatplotlib(unittest.TestCase):
-    def test_get_font_path(self):
-        is_font_path = os.path.exists(get_font_path())
-        self.assertEqual(is_font_path, True)
-
-    def test_get_font_ttf_path(self):
-        is_ttf_path = os.path.exists(get_font_ttf_path())
-        self.assertEqual(is_ttf_path, True)
-
-    def test_font_manager_ttf_path(self):
-        ttf_path = font_manager.fontManager.ttflist[-1].fname
-        self.assertEqual(ttf_path, get_font_ttf_path())
+def test_get_font_path():
+    is_font_path = os.path.exists(get_font_path())
+    assert is_font_path is True
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_get_font_ttf_path():
+    is_ttf_path = os.path.exists(get_font_ttf_path())
+    assert is_ttf_path is True
+
+
+def test_font_manager_ttf_path():
+    ttf_path = font_manager.fontManager.ttflist[-1].fname
+    assert ttf_path == get_font_ttf_path()
